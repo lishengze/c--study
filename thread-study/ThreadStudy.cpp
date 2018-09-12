03c34c3375e7f84475e8198a416e7a3d3a1c54ca
lishengze <lishengz.cool@hotmail.com>
2018-09-12 08:38:07 +0800

    add thread_study

---
+++ thread-study/ThreadStudy.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : ThreadStudy.cpp
+// Author      : lsz
+// Version     :
+// Copyright   : None
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "thread_func.h"
+using namespace std;
+
+int main() {
+	test_thread_func_main();
+	return 0;
+}
