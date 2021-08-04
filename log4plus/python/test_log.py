5302ba37584b1dd3bee3c3597d2052acec5ee63f
lishengze <lishengz.cool@hotmail.com>
2021-08-04 20:07:19 +0800

    add log4plus

--- log4plus/python/test_log.py
+++ log4plus/python/test_log.py
@@ -1,6 +1,7 @@
 import logging
 import logging.handlers
 import datetime
+import time
 
 def basic_test():
     # logging.basicConfig(level=logging.DEBUG)
@@ -34,11 +35,92 @@ def upgrade_test():
     logger.info('info message')
     logger.warning('warning message')
     logger.error('error message')
-    logger.critical('critical message')    
+    logger.critical('critical message')
+
+def produce_test():
+    LOG_FORMAT = "%(pathname)s-%(lineno)s - %(asctime)s - %(levelname)s - %(message)s"
+    DATE_FORMAT = "%m/%d/%Y %H:%M:%S"    
+    logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
+    logger = logging.getLogger('user_logger')
+    logger.setLevel(logging.DEBUG)
+
+    # detail_handler = logging.handlers.TimedRotatingFileHandler('log/all.log', when='midnight', interval=1, backupCount=7, atTime=datetime.time(0, 0, 0, 0))
+
+    detail_handler = logging.handlers.RotatingFileHandler('log/all.log', maxBytes=8*1024*1024*500, backupCount=5)
+    
+    detail_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+
+    # error_handler = logging.FileHandler('log/error.log')
+
+    error_handler = logging.handlers.RotatingFileHandler('log/error.log', maxBytes=8*1024*1024*500, backupCount=5)
+
+    error_handler.setLevel(logging.WARNING)
+    error_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+
+    logger.addHandler(detail_handler)
+    logger.addHandler(error_handler)
+
+    logger.debug('debug message %s' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    logger.info('info message %s' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    logger.warning('warning message %s' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    logger.error('error message %s' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    logger.critical('critical message %s' % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))        
+
+class Logger(object):
+    def __init__(self):
+        LOG_FORMAT = "%(pathname)s-%(lineno)s - %(asctime)s - %(levelname)s - %(message)s"
+        DATE_FORMAT = "%Y/%m/%d %H:%M:%S"    
+        logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+
+        self._logger = logging.getLogger('user_logger')
+        self._logger.setLevel(logging.DEBUG)
+
+        # detail_handler = logging.handlers.TimedRotatingFileHandler('log/all.log', when='midnight', interval=1, backupCount=7, atTime=datetime.time(0, 0, 0, 0))
+
+        detail_handler = logging.handlers.RotatingFileHandler('log/all.log', maxBytes=8*1024*1024*500, backupCount=3)
+        
+        detail_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+
+        # error_handler = logging.FileHandler('log/error.log')
+
+        error_handler = logging.handlers.RotatingFileHandler('log/error.log', maxBytes=8*1024*1024*500, backupCount=3)
+
+        error_handler.setLevel(logging.WARNING)
+        error_handler.setFormatter(logging.Formatter(fmt="%(asctime)s - %(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+
+        self._logger.addHandler(detail_handler)
+        self._logger.addHandler(error_handler)        
+
+    def Debug(self, info):
+        self._logger.debug(info)
+
+    def Info(self, info):
+        self._logger.info(info)
+
+    def Warning(self, info):
+        self._logger.warning(info)
+
+    def Error(self, info):
+        self._logger.error(info)
+
+    def Critical(self, info):
+        self._logger.critical(info)          
+
+def test_logger():
+    logger = Logger();  
+
+    logger.Debug("test debug %s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    logger.Info("test info %s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    logger.Warning("test warning %s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    logger.Error("test error %s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
+    logger.Critical("test critical %s" % (time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
 
 def main():
     # basic_test()
-    upgrade_test()
+    # upgrade_test()
+    # produce_test()
+    test_logger()
 
 if __name__ == '__main__':
     main()
