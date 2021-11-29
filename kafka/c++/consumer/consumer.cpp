a37bbeef8853d5fd1486be258610097850ca93d1
lishengze <lishengz.cool@hotmail.com>
2021-11-29 18:36:35 +0800

    test create topic

--- kafka/c++/consumer/consumer.cpp
+++ kafka/c++/consumer/consumer.cpp
@@ -1,9 +1,84 @@
 #include "kafka/KafkaConsumer.h"
+#include "kafka/KafkaConsumer.h"
+#include "kafka/KafkaProducer.h"
+#include "kafka/AdminClient.h"
 
 #include <iostream>
 #include <string>
+using namespace std;
 
-int main(int argc, char **argv)
+
+kafka::Topics get_created_topics(kafka::clients::AdminClient& adclient)
+{
+    kafka::Topics result;
+
+    try
+    {
+        auto listResult = adclient.listTopics();
+        
+        if (listResult.error)
+        {
+            cout << listResult.error.message() << endl;
+        }
+        else
+        {
+            result = listResult.topics;
+
+            for (auto topic:result)
+            {
+                cout << topic << "\t";
+            }
+            cout << endl;
+
+            return result;
+        }    
+
+        return result;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    return result;
+}
+
+void test_create_topic()
+{
+    try
+    {
+        kafka::Properties adclient_props ({
+            {"bootstrap.servers",  "127.0.0.1:9092"}
+        });    
+
+        kafka::clients::AdminClient adclient(adclient_props);
+
+        cout << "original topics: " << endl;
+        kafka::Topics original_topics = get_created_topics(adclient);
+
+        // kafka::Properties topic_props ({
+        //     {"max.message.bytes",  "1048588"}
+        // });
+
+        kafka::Properties topic_props;
+
+        auto createResult = adclient.createTopics({"Cplus_Topic"}, 3, 3, topic_props);
+        if (createResult.error)
+        {
+            cout << "Create Topic Error: " + createResult.error.message() << endl;
+            return;
+        }
+
+        cout << "new topics: " << endl;
+        kafka::Topics new_topics = get_created_topics(adclient);        
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+int test_consumer()
 {
     std::string brokers = "127.0.0.1:9092";
     kafka::Topic topic  = "test_topic";
@@ -60,5 +135,12 @@ int main(int argc, char **argv)
     } catch (const kafka::KafkaException& e) {
         std::cerr << "% Unexpected exception caught: " << e.what() << std::endl;
     }
+}
+
+int main(int argc, char **argv)
+{
+    test_create_topic();
+
+    return 1;
 }
 
---
+++ kafka/python/client.py
@@ -0,0 +1,109 @@
+from kafka import KafkaProducer
+from kafka import KafkaConsumer
+from kafka import TopicPartition
+# from kafka import KafkaAdminClient
+from kafka.admin import KafkaAdminClient, NewTopic
+import kafka
+
+import datetime
+import time
+import os
+import json
+
+from kafka.consumer import group
+
+def get_datetime_str():
+    return datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
+
+def test_producer():
+    try:
+        producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9093'])
+        if producer.bootstrap_connected():
+            topic_name = 'test_topic'
+
+            # msg = "python producer " + get_datetime_str()
+            # msg.encode('utf-8')
+            # print("send: " + msg)
+            # producer.send(topic_name, value=msg, key=None, headers=None, partition=None, timestamp_ms=None)
+
+            i = 1
+            while i <= 5:
+                i += 1
+                # msg = "producer_%d" % i
+                msg = "producer_%s" % get_datetime_str()
+
+                print(msg)
+                # producer.send(topic_name, value=b"test_again")
+                producer.send(topic_name, value=bytes(msg.encode()))
+                time.sleep(3)
+
+    except Exception as e:
+        print("exception: " + str(e))
+
+def test_consumer():
+    try:
+        topic_name = 'test_topic'
+        server_list = ['127.0.0.1:9117']
+
+        consumer = KafkaConsumer(bootstrap_servers=server_list,
+                                 auto_offset_reset='latest',
+                                 group_id="test_consumer_1")
+
+
+        # consumer = KafkaConsumer(topic_name,
+        #                         bootstrap_servers=server_list,
+        #                         auto_offset_reset='latest',# 消费kafka中最近的数据，如果设置为earliest则消费最早的数据，不管这些数据是否消费
+        #                         enable_auto_commit=True, # 自动提交消费者的offset
+        #                         auto_commit_interval_ms=3000, ## 自动提交消费者offset的时间间隔
+        #                         group_id='MY_GROUP1',
+        #                         consumer_timeout_ms= 10000, # 如果10秒内kafka中没有可供消费的数据，自动退出
+        #                         client_id='consumer-python3'
+        #                         )
+
+        if consumer.bootstrap_connected():            
+            print("consumer connect server: %s successfully!" % (str(server_list)))
+            print(consumer.topics())
+            consumer.subscribe(topics=topic_name)
+
+            print(consumer.subscription())
+
+            i = 0
+            for msg in consumer:
+                print(i)
+                i+=1
+                print(msg.value)
+        else:
+            print("consumer connect server: %s failed!" % (str(server_list)))
+
+            # print(consumer.metrics())
+
+
+    except Exception as e:
+        print("exception: " + str(e))
+
+def test_client():
+    print("Test CreateTopics")
+    
+    server_list = "localhost:9092"
+    consumer = KafkaConsumer(bootstrap_servers=server_list,
+                            auto_offset_reset='latest',
+                            group_id="test_consumer_1")
+    
+    print("Before Create Topic: \n%s" % (str(consumer.topics())))
+        
+    admin_client =KafkaAdminClient(bootstrap_servers=server_list, client_id='test')
+    
+    topic_list = []
+    topic_list.append(NewTopic(name="example_topic", num_partitions=1, replication_factor=1))
+    admin_client.create_topics(new_topics=topic_list, validate_only=False)
+    
+    print("After Create Topic: \n%s" % (str(consumer.topics())))
+
+
+def test_main():
+    # test_producer()
+    # test_consumer()
+    test_client()
+
+if __name__ == "__main__":
+    test_main()
