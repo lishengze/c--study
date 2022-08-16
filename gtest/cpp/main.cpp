80d1be7bdf7146f6675da56339f9dbe861d10077
lishengze <lishengz.cool@hotmail.com>
2022-08-16 15:34:02 +0800

    update

---
+++ gtest/cpp/main.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : test google test
+// Author      : lsz
+// Version     :
+// Copyright   : None
+// Description : Study right refrenece in C++, Ansi-style
+// Date        : 2022-8-16
+//============================================================================
+
+#include "base.h"
+#include <stdio.h>
+
+int main() 
+{
+	printf("Test Google");
+
+	return 0;
+}
