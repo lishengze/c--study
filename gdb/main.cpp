3459945437e44fa8a48db7a5b5182ba0a06c2971
lishengze <lishengz.cool@hotmail.com>
2022-11-23 18:53:51 +0800

    check gcc

--- gdb/main.cpp
+++ gdb/main.cpp
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
-int func(int* p) {
-    int y = *p;
-    return y;
+// int func(int* p) {
+//     int y = *p;
+//     return y;
+// }
+
+// int main() {
+    
+//     int* p = NULL;
+
+//     int a = func(p);
+
+//     return a;
+// }
+
+int add_a_and_b(int a, int b) {
+return a + b;
 }
 
 int main() {
-    
-    int* p = NULL;
-
-    int a = func(p);
-
-    return a;
+return add_a_and_b(2, 3);
 }
