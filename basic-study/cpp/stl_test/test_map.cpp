f21b358a65f1d13ab9deca73e166d43a5da44813
lsz <lsz>
2024-09-13 13:50:38 +0800

    测试 unordered_map

--- basic-study/cpp/stl_test/test_map.cpp
+++ basic-study/cpp/stl_test/test_map.cpp
@@ -1,6 +1,9 @@
 #include "test_map.h"
 #include <map>
+#include <unordered_map>
 #include <string>
+#include <stdio.h>
+#include <string.h>
 using std::map;
 using std::string;
 
@@ -14,5 +17,25 @@ void test_string_key()
 
 void test_int_key()
 {
+    
+}
+
+void test_unorder_map() {
+    struct AtomTest {
+        double dvalue1_;
+        double dvalue2_;
+    };
+
+    struct UMap {
+        int iValue1_;
+        double dValue2_;
+        // std::unordered_map<std::string, AtomTest> stValue_;
+        std::map<std::string, AtomTest> stValue_;
+    };
+
+    UMap stData1;
+    UMap stData2;
+
+    memcpy(&stData2,&stData1,sizeof(UMap));
     
 }
