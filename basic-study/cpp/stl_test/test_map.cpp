f053fc965cd20e927966560e5a448c0ab02ff58c
lsz <lsz>
2024-09-13 13:50:58 +0800

    完成unordermap 基础测试

--- basic-study/cpp/stl_test/test_map.cpp
+++ basic-study/cpp/stl_test/test_map.cpp
@@ -7,12 +7,12 @@
 using std::map;
 using std::string;
 
-// 测试 以 string 和 int 分别做key, 高速插入，删除的性能比较；
-// 10个交易所， 100种币对， 2个买卖方向;
+// 测试 �? string �? int 分别做key, 高速插入，删除的性能比较�?
+// 10�?交易所�? 100种币对， 2�?买卖方向;
 
 void test_string_key()
 {
-    map<string, map<string, map<string, int>>>      string_map;
+    std::map<string, map<string, map<string, int>>>      string_map;
 }
 
 void test_int_key()
@@ -27,15 +27,26 @@ void test_unorder_map() {
     };
 
     struct UMap {
-        int iValue1_;
-        double dValue2_;
+        // int iValue1_;
+        // double dValue2_;
         // std::unordered_map<std::string, AtomTest> stValue_;
         std::map<std::string, AtomTest> stValue_;
     };
 
+    struct UnOrderMap {
+        // int iValue1_;
+        // double dValue2_;
+        std::unordered_map<std::string, AtomTest> stValue_;
+        // std::map<std::string, AtomTest> stValue_;
+    };
+
+
     UMap stData1;
     UMap stData2;
 
-    memcpy(&stData2,&stData1,sizeof(UMap));
+    // memcpy(&stData2,&stData1,sizeof(UMap));
+
+    printf("map.size: %ld, unorder_map.size: %ld \n", sizeof(UMap), sizeof(UnOrderMap));
+
     
 }
