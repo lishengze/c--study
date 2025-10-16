909a5dc0649c1e5cc11b295e15e64001e842c8c0
th_ubu <th_ubu>
2025-10-16 20:47:48 +0800

    升级demo

---
+++ queue/demo_2025_10_15/rsp_queue.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class RspQueueManager {    
+public:
+    RspQueueManager(int size) {
+        // m_size = size;
+        // m_buffer = new char[m_size];
+    }
+};
---
+++ queue/demo_2025_10_15/strategy_message_manager.cpp
@@ -0,0 +1,10 @@
+#include "strategy_message_manager.h"
+
+bool StrategyMessageManager::SendMsg(int iMsgID, const char* pMsgBuf, const int iMsgLen) {
+    // TODO: Implement the message sending logic here.
+    return true;
+}
+
+bool StrategyMessageManager::Init(const char* UteName, unsigned int StrategySysID) {
+    return true;
+}
